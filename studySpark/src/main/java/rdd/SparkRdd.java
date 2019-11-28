1d633a9449675eefa3e8c29cb4063f40539d1df1
sihai <2693711535@qq.com>
2019-11-28 16:21:01 +0800

    study spark

---
+++ studySpark/src/main/java/rdd/SparkRdd.java
@@ -0,0 +1,22 @@
+package rdd;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+public class SparkRdd {
+  public static void main(String[] args) {
+    SparkConf conf = new SparkConf()
+        .setAppName("sike test app")
+        .setMaster("local[3]")
+        .set("spark.eventLog.enabled", "true")
+        .set("spark.eventLog.dir", "/home/sike/src/testData/SparkEvents")
+        .set("spark.history.fs.logDirectory", "/home/sike/src/testData/SparkEvents");
+    JavaSparkContext sc = new JavaSparkContext(conf);
+    JavaRDD<String> distFile = sc.textFile("/home/sike/src/testData/a.txt");
+    JavaRDD<Integer> lineLength = distFile.map(s -> s.length());
+    int totalLength = lineLength.reduce((a, b) -> a + b);
+    System.out.println("total length is : " + totalLength);
+    sc.stop();
+  }
+}
