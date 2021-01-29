26c293d1bc9b2470627b76ccd1fb8ae8c8580fab
kesihai <2693711535@qq.com>
2021-01-29 19:39:51 +0800

    refine cide

--- studyHadoop/src/main/java/studySpark/stream/SparkStreamWordCount.java
+++ studyHadoop/src/main/java/studySpark/stream/SparkStreamWordCount.java
@@ -5,16 +5,25 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.streaming.OutputMode;
+import org.apache.spark.sql.streaming.StreamingQuery;
+import org.apache.spark.sql.streaming.StreamingQueryException;
 
 import java.util.Arrays;
 
 public class SparkStreamWordCount {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws StreamingQueryException {
     SparkSession spark = SparkSession.builder()
+        .master("local[2]")
         .appName("javaStructuredWordCount")
         .getOrCreate();
     Dataset<Row> lines = spark.readStream().format("socket").option("host", "localhost").option("port", 9999).load();
-    Dataset<String> words = lines.as(Encoders.STRING()).flatMap((FlatMapFunction<String, String>)x -> Arrays.asList(x.split(",")).iterator(), Encoders.STRING());
-    
+    Dataset<String> words = lines.as(Encoders.STRING()).flatMap((FlatMapFunction<String, String>)x -> Arrays.asList(x.split(" ")).iterator(), Encoders.STRING());
+    Dataset<Row> wordCounts = words.groupBy("value").count();
+    StreamingQuery query = wordCounts.writeStream()
+        .format("console")
+        .outputMode(OutputMode.Complete())
+        .start();
+    query.awaitTermination();
   }
 }
